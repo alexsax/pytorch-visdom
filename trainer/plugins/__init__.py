0529b1608a15a73fa35326d3909024a17e2ecbc6
sasha <asax@stanford.edu>
2017-07-31 13:52:35 -0700

    added basic visdom logging

--- trainer/plugins/__init__.py
+++ trainer/plugins/__init__.py
@@ -3,3 +3,4 @@ from .accuracy import AccuracyMonitor
 from .time import TimeMonitor
 from .loss import LossMonitor
 from .logger import Logger
+from .visdom_logger import VisdomLogger, VisdomPlotLogger, VisdomTextLogger
